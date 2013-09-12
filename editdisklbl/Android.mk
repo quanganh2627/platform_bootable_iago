c673fa6cb478d7f8416f5952eaf9b4dfedf3e7c6
Andrew Boie <andrew.p.boie@intel.com>
2013-09-12 15:38:42 -0700

    copy libdiskconfig code directly into editdisklbl
    We originally copied editdisklbl code so that upstream changes
    wouldn't break us. Doing the same thing with the supporting
    libdiskconfig code. Google has been very slow to review our
    changes, and since we only use this library for a single very
    specific purpose, just copy it here.
    
    Issue: ADEV-21
    Change-Id: I8efe497fe5de34105c4f8f51ef6863d396dbf933
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- editdisklbl/Android.mk
+++ editdisklbl/Android.mk
@@ -5,15 +5,17 @@ include $(CLEAR_VARS)
 ifeq ($(TARGET_ARCH),x86)
 
 LOCAL_SRC_FILES := \
-	editdisklbl.c
+	editdisklbl.c \
+        config_mbr.c \
+        diskconfig.c \
+        diskutils.c \
+        write_lst.c
 
-LOCAL_C_INCLUDES := \
-	$(LOCAL_PATH)/../libdiskconfig
 
 LOCAL_CFLAGS := -O2 -g -W -Wall -Werror -D_LARGEFILE64_SOURCE
 
 LOCAL_MODULE := editdisklbl
-LOCAL_STATIC_LIBRARIES := libdiskconfig_host libcutils liblog
+LOCAL_STATIC_LIBRARIES := libcutils liblog
 
 include $(BUILD_HOST_EXECUTABLE)
 
